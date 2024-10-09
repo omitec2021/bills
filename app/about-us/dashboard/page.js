9bfd428af52978386ef96d8028620e8f3a4c7f37
Agiobu Omiete <omitec2021@gmail.com>
2024-10-09 13:45:11 +0100

    UI design

--- app/about-us/dashboard/page.js
+++ app/about-us/dashboard/page.js
@@ -2,6 +2,9 @@ export default function Dashboard () {
     return (
         <main>
             <p>User dashboard</p>
+            <p>Propose a UI design</p>
+            <p>Build UI</p>
+            <p>Propose possible changes</p>
         </main>
     )
 }
